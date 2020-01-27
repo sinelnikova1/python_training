5605a939ccdbb1caf3df17ff6c756a93ff99bc61
ASinelnikova <^7bz4=*RfD2XWx[s>
2020-01-27 19:23:50 +0300

    optimization of the transitions between pages

--- fixture/contacts.py
+++ fixture/contacts.py
@@ -8,7 +8,8 @@ class ContactsHelper:
 
     def open_add_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("firstname")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def add_information_of_person(self, contacts):
         wd = self.app.wd
