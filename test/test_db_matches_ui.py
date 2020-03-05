000ed0c21bec70b410da612122fb59b651e63d9b
ASinelnikova <^7bz4=*RfD2XWx[s>
2020-03-05 15:50:54 +0300

    check matches ui and db

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -5,6 +5,6 @@ from timeit import timeit
 def test_group_list(app, db):
     print(timeit(lambda: app.group.get_group_list(), number=1))
     def clean(group):
-        return Group(id=group.id, name = group.name.strip())
-    print(timeit(lambda: map(clean,db.get_group_list()), number=1000))
+        return Group(id=group.id, name=group.name.strip())
+    print(timeit(lambda: map(clean, db.get_group_list()), number=1000))
     assert False #sorted(ui_list, key = Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
--- test/test_db_matches_ui_contact.py
+++ test/test_db_matches_ui_contact.py
@@ -1,10 +1,9 @@
 from model.contacts import Contacts
-from timeit import timeit
 
 
-def test_contact_list(app, db, check_ui):
-    print(timeit(lambda: app.group.get_contacts_list(), number=1))
+def test_contact_list(app, db):
+    check_ui = app.contacts.get_contacts_list()
     def clean(contact):
-        return Contacts(id=contact.id, name = contact.firstname.strip())
-    print(timeit(lambda: map(clean,db.get_contacts_list()), number=1000))
-    assert False #sorted(ui_list, key = Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+        return Contacts(id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip())
+    check_db = map(clean, db.get_contacts_list())
+    assert sorted(check_ui, key=Contacts.id_or_max) == sorted(check_db, key=Contacts.id_or_max)
