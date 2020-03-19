00a4a7cf2bc1fd82d3d9129ecd413c5a2f80de20
ASinelnikova <^7bz4=*RfD2XWx[s>
2020-03-19 23:00:08 +0300

    add test allure (other test in past commit)

--- test/test_contacts_db/test_add_contact.py
+++ test/test_contacts_db/test_add_contact.py
@@ -1,20 +1,23 @@
 from model.contacts import Contacts
-
+import allure
 
 #@pytest.mark.parametrize("contact", testdata, ids=[str(x) for x in testdata])
 
+
 def test_add_contact(app, db, json_contacts, check_ui):
     contact = json_contacts
-    old_contacts = db.get_contacts_list()
-    app.contacts.add_information_of_person(contact)
-    assert len(old_contacts) + 1 == app.contacts.count()
-    new_contacts = db.get_contacts_list()
-    # проверка сравнения старого и нового списка с добавлением элемента
-    old_contacts.append(contact)
-    #assert old_contacts == new_contacts
-    if check_ui:
-     assert sorted(new_contacts, key=Contacts.id_or_max) == sorted(app.contacts.get_contacts_list(), key=Contacts.id_or_max)
-
+    with allure.step('Given a contact list'):
+        old_contacts = db.get_contacts_list()
+    with allure.step('When I add a contact %s to the list' % contact):
+        app.contacts.add_information_of_person(contact)
+    with allure.step('Then the new contact list is equal to the old list with added contact'):
+        assert len(old_contacts) + 1 == app.contacts.count()
+        new_contacts = db.get_contacts_list()
+        # проверка сравнения старого и нового списка с добавлением элемента
+        old_contacts.append(contact)
+        #assert old_contacts == new_contacts
+        if check_ui:
+         assert sorted(new_contacts, key=Contacts.id_or_max) == sorted(app.contacts.get_contacts_list(), key=Contacts.id_or_max)
 
 
 
